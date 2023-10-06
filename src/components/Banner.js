77d5e81817054ecc0d224041ddf2fcf3caeaa460
HARSH SHUKLA <HARSHSHUKLA2022@VITBHOPAL.AC.IN>
2023-10-06 20:33:01 +0530

    Updated Responsiveness

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -53,25 +53,62 @@ export const Banner = () => {
         <Row className="aligh-items-center">
           <Col xs={12} md={6} xl={7}>
             <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <span className="tagline">Welcome to Omdena VITB Chapter</span>
-                <h1>{`Artificiail Inteligence`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "For Impact", "To Change the World" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                  <button onClick={() => window.open("https://www.linkedin.com/in/harsh-shukla-1inkdein/", "_blank")}className="connectBtn">Let’s Connect <ArrowRightCircle size={25} /></button>
-              </div>}
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <span className="tagline">
+                    Welcome to Omdena VITB Chapter
+                  </span>
+                  <h1>
+                    {`Artificial Intelligence`}{" "}
+                    <span
+                      className="txt-rotate"
+                      dataPeriod="1000"
+                      data-rotate='[ "For Impact", "To Change the World" ]'
+                    >
+                      <span className="wrap">{text}</span>
+                    </span>
+                  </h1>
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book.
+                  </p>
+                  <button
+                    onClick={() =>
+                      window.open(
+                        "https://www.linkedin.com/in/harsh-shukla-1inkdein/",
+                        "_blank"
+                      )
+                    }
+                    className="connectBtn"
+                  >
+                    Let’s Connect <ArrowRightCircle size={25} />
+                  </button>
+                </div>
+              )}
             </TrackVisibility>
           </Col>
           <Col xs={12} md={6} xl={5}>
             <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
-                  <img src={headerImg} alt="Header Img"/>
-                </div>}
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__zoomIn" : ""
+                  }
+                >
+                  <img src={headerImg} alt="Header Img" />
+                </div>
+              )}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
     </section>
-  )
+  );
 }
